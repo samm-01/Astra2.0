a92923b2331377da47aab04487451d60c797620f
samm-01 <sameergarg006@gmail.com>
2023-02-27 23:04:57 +0530

    User Authentication & Password Encryption

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 const md5 = require("md5")
 const bcrypt = require("bcrypt");
-// const saltRounds = 10;
+const saltRounds = 10;
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
@@ -20,6 +20,17 @@ app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+app.use(session({
+    secret: "Our Little Secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/astraUserDB", { useNewUrlParser: true });
 mongoose.set('strictQuery', true);
 
@@ -31,10 +42,24 @@ const astraUserSchema = new mongoose.Schema({
     body: String,
 });
 
+astraUserSchema.plugin(passportLocalMongoose);
+astraUserSchema.plugin(findOrCreate);
+
 const astraUser = new mongoose.model("astraUser", astraUserSchema);
 const userPost = new mongoose.model("userPost", astraUserSchema);
-var x;
-// passport.use(astraUser.createStrategy());
+
+passport.use(astraUser.createStrategy());
+
+passport.serializeUser(function (astraUser, done) {
+    done(null, astraUser.id);
+});
+passport.deserializeUser(function (id, done) {
+    astraUser.findById(id, function (err, astraUser) {
+        done(err, astraUser);
+    });
+});
+
+// astraUserSchema.plugin(encrypt, { encryptedFields: ["password"] });
 
 
 app.get("/", function (req, res) {
@@ -65,36 +90,71 @@ app.get("/check", function (req, res) {
     res.render("check");
 });
 
+app.get('/logout', function (req, res) {
+    req.logout(function (err) {
+        if (err) {
+            return err
+        }
+        res.redirect('/');
+    });
+});
 
 
 app.post("/register", function (req, res) {
-    const newUser = new astraUser({
-        email: req.body.username,
-        password: (req.body.password)
-    });
-    newUser.save(function (err) {
+
+    // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    //     const newUser = new astraUser({
+    //         email: req.body.username,
+    //         // password: md5(req.body.password),
+    //         password: hash
+    //     });
+    //     newUser.save(function (err) {
+    //         if (err) {
+    //             console.log(err);
+    //         } else {
+    //             res.render("posts");
+    //         }
+    //     });
+    // });
+    astraUser.register({ username: req.body.username }, req.body.password, function (err, astraUser) {
         if (err) {
             console.log(err);
+            res.redirect("/register");
         } else {
-            // res.render("secrets");
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/posts");
+            });
         }
     });
 });
 
 app.post("/login", function (req, res) {
-    const username = req.body.username;
-    const password = (req.body.password)
-
-    astraUser.findOne({ email: username }, function (err, foundUser) {
+    const astrauser = new astraUser({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(astrauser, function (err) {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser) {
-                console.log(foundUser.email);
+            passport.authenticate("local")(req, res, function () {
                 res.redirect("/posts");
-            }
+            });
         }
-    });
+    })
+    // const username = req.body.username;
+    // const password = (req.body.password)
+
+    // astraUser.findOne({ email: username }, function (err, foundUser) {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         if (foundUser) {
+    //             console.log(foundUser.email);
+    //             res.redirect("/posts");
+    //         }
+    //     }
+    // });
 });
 app.post("/posts", function (req, res) {
     const newPost = new userPost({
